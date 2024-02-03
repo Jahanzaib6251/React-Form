aae5d302208be1673af75423b78111dcf256bb2e
jahanzaib6251 <jahanzaib.6251@gmail.com>
2024-02-03 15:27:25 +0500

    initial commit 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Form from './Components/Form';
+import Test from './Components/Test';
 
 function App() {
   return (
     <div className="App">
       **************React App**************
       <Form></Form>
+      <Test></Test>
     </div>
   );
 }
---
+++ src/Components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>Test The Git Hub</div>
+  )
+}
+
+export default Test
